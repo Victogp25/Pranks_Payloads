c60242fbe066c1b7d6430910ec9f3d60fdf0d78c
Victogp25 <136272736+Victogp25@users.noreply.github.com>
2025-03-16 21:37:42 -0600

    RickRoll on Youtube.py

--- Gato_giratorio.py
+++ Gato_giratorio.py
@@ -1,3 +1,55 @@
-import webbrowser
+REM     Title: Gato Giratorio Troll
 
-webbrowser.open('https://www.youtube.com/watch?v=U2Fjfqm-7g8&t')
+REM     Author: Tú
+
+REM     Description: Este payload abre el video del gato giratorio en YouTube para gastarle una broma a tu amigo.
+
+REM     Target: Windows 10, 11
+
+REM     --------------------------------------------------------------------------------------
+REM     ESTE PAYLOAD ES PLUG AND PLAY. NO MODIFICACIONES NECESARIAS, SIMPLEMENTE EJECUTA EL CÓDIGO DE ABAJO.
+REM     --------------------------------------------------------------------------------------
+
+
+REM     Abrir el navegador y reproducir el video
+GUI r
+DELAY 500
+STRING firefox.exe 
+ENTER
+DELAY 1000
+STRING https://www.youtube.com/watch?v=dQw4w9WgXcQ
+DELAY 500
+ENTER
+DELAY 5000
+
+REM     Poner el video en pantalla completa
+K
+DELAY 500
+FREM     Title: Gato Giratorio Troll
+
+REM     Author: Tú
+
+REM     Description: Este payload abre el video del gato giratorio en YouTube para gastarle una broma a tu amigo.
+
+REM     Target: Windows 10, 11
+
+REM     --------------------------------------------------------------------------------------
+REM     ESTE PAYLOAD ES PLUG AND PLAY. NO MODIFICACIONES NECESARIAS, SIMPLEMENTE EJECUTA EL CÓDIGO DE ABAJO.
+REM     --------------------------------------------------------------------------------------
+
+
+REM     Abrir el navegador y reproducir el video
+GUI r
+DELAY 500
+STRING firefox.exe 
+ENTER
+DELAY 1000
+STRING https://www.youtube.com/watch?v=dQw4w9WgXcQ
+DELAY 500
+ENTER
+DELAY 5000
+
+REM     Poner el video en pantalla completa
+K
+DELAY 500
+F
